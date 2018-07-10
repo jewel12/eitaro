9e2e0e0a1240ce2ed38bb5e002a6cb5ed95bdf9e
jewel12 <xiedajeeling@gmail.com>
2018-07-11 01:15:55 +0900

    animation

--- main.go
+++ main.go
@@ -2,18 +2,31 @@ package main
 
 import (
   "syscall/js"
+  "math/rand"
   "time"
-  "fmt"
   "strconv"
+  "fmt"
 )
 
 func main() {
-  body := js.Global().Get("document").Get("body")
-  style := body.Get("style")
+  t := time.Tick(100 * time.Millisecond)
 
-  deg := time.Now().UnixNano() % 360
+  var deg int64
+  var cn int
+  for {
+    fmt.Printf("%d", js.Global().Get("alpha"))
+    select {
+      case <-t:
+        deg = 8 + (deg % 360)
+        if rand.Intn(10) == 0 {
+          cn  = rand.Intn(16777215)
+        }
+	c := fmt.Sprintf("#%06X", cn)
 
-  background := "linear-gradient(" + strconv.FormatInt(deg, 10) + "deg, #00BFFF 25%, #CEECF5 25%, #CEECF5 50%, #00BFFF 50%, #00BFFF 75%, #CEECF5 75%, #CEECF5)"
-  fmt.Println(deg)
-  style.Set("backgroundImage", background)
+        body := js.Global().Get("document").Get("body")
+        style := body.Get("style")
+        background := "linear-gradient(" + strconv.FormatInt(deg, 10) + "deg, " + c + " 25%, #CEECF5 25%, #CEECF5 50%," + c + " 50%, " + c + " 75%, #CEECF5 75%, #CEECF5)"
+        style.Set("backgroundImage", background)
+    }
+  }
 }
